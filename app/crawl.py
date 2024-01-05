312c12c3d5d2c424caca3f9f57a28118f1235022
Zander Buteux <118382806+ZBbop@users.noreply.github.com>
2024-01-05 10:52:37 -0700

    bug clean up
    stricter for text/html content-types, was getting caught up on turbo-streams
    
    indentation of link_dict append was off so it wasn't comprehensive

--- app/crawl.py
+++ app/crawl.py
@@ -129,148 +129,149 @@ while urls_to_crawl:
         continue
     elif 'wp-content/' in url:
         continue
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    # Store the DOM in a variable
-    dom = response.text
-    # Check if in sitemap
-    sitemap_bool = 0
-    if url in sitemap_contents:
-      sitemap_bool = 1
-    else:
+    elif 'Content-Type' in requests.get(url).headers and 'text/html' in requests.get(url).headers['Content-Type']:
+      response = requests.get(url)
+      soup = BeautifulSoup(response.content, 'html.parser')
+      # Store the DOM in a variable
+      dom = response.text
+      # Check if in sitemap
       sitemap_bool = 0
-
-    print(url)    
-    # Remove the URL from the urls_to_crawl array
-    if url in urls_to_crawl:
-        urls_to_crawl.remove(url)
-    # Add it to the urls_crawled array
-    urls_crawled.append(url)
-
-    # get and check links
-    out_links = 0
-    in_links = 0
-    redirect_links = 0
-    links_404 = 0
-    lnx = soup.find_all('a')
-    links_on_page = len(lnx)
-    link_href = []
-    link_anchor = []
-    for link in lnx:
-      l = link.get('href')
-      t = link.get_text()
-      if l.startswith('/'):
-        l = f"{scheme}{root_domain}{l}"
-      if root_domain in l:
-        if l not in urls_to_crawl and l not in urls_crawled:
-          if '#' not in l:
-            if '/wp-content/' not in l:
-              link_url_status = get_url_status(link)
-              urls_to_crawl.add(l)
-        in_links += 1
+      if url in sitemap_contents:
+        sitemap_bool = 1
       else:
-        out_links += 1
-      if link_url_status == 301 or link_url_status == 302:
-        redirect_links += 1
-      elif link_url_status == 404:
-        links_404 += 1
-      if t and l:
-        link_dict = {
-           'url crawled': url,
-           'link_anchor': t,
-           'link_href': l,
-           'link_url_status': link_url_status
-        }
-
-    # links to array for export
-    links_data.append(link_dict)
-      
-    # parse the heading tags
-    h1_raw = soup.find_all('h1')
-    h1 = []
-    h2_raw = soup.find_all('h2')
-    h2 = []
-    h3_raw = soup.find_all('h3')
-    h3 = []
-
-    # extract the strings and add to arrays
-    for h in h1_raw:
-      heading = str(h.string)
-      h1.append(heading)
-    for h in h2_raw:
-      heading = str(h.string)
-      h2.append(heading)
-    for h in h3_raw:
-      heading = str(h.string)
-      h3.append(heading)
-
-    # Extract page title
-    if soup.title.string:
-      page_title = str(soup.title.string)
-    else:
-      page_title = 'No Title'
-    
-    # Extract all meta tags
-    meta_tags = soup.find_all('meta')
-    meta_tag_names = []
-    meta_content_names = []
-    for meta in meta_tags:
-      if not meta.get('name'):
-        continue
-      else: 
-        meta_tag_names.append(meta.get('name'))
-        meta_content_names.append(meta.get('content'))
-
-    #output to array
-    crawl_overview_data.append({
-     'url crawled': url,
-     'url_status': url_status,
-     'sitemap_bool': sitemap_bool,
-     'out_links': out_links,
-     'in_links': in_links,
-     'redirect_links': redirects,
-     'links_404': links_404,
-     'page_title': page_title,
-     'h1': h1,
-     'h2': h2,
-     'h3': h3
-    })
-
-    # Write crawl_overview_data and links_data to CSV files
-    if crawl_overview_data:
-        with open('crawl_overview.csv', 'a', newline='', encoding='utf-8') as file:
-            writer = csv.DictWriter(file, fieldnames=crawl_overview_data[0].keys())
-            if not os.path.isfile('crawl_overview.csv') or os.stat('crawl_overview.csv').st_size == 0:
-                writer.writeheader()
-            writer.writerows(crawl_overview_data)
-    else:
-        print("No data to write to CSV.")
+        sitemap_bool = 0
   
-    if links_data:
-        with open('links.csv', 'a', newline='', encoding='utf-8') as file:
-            writer = csv.DictWriter(file, fieldnames=links_data[0].keys())
-            if not os.path.isfile('links.csv') or os.stat('links.csv').st_size == 0:
-                writer.writeheader()
-            writer.writerows(links_data)
-    else:
-        print("No data to write to CSV.")
-
-    if urls_to_crawl:
-        with open('urls_to_crawl.csv', 'a', newline='', encoding='utf-8') as file:
-            writer = csv.DictWriter(file, fieldnames=['URL'])
-            if not os.path.isfile('urls_to_crawl.csv') or os.stat('urls_to_crawl.csv').st_size == 0:
-                writer.writeheader()
-            for url in urls_to_crawl:
-                writer.writerow({'URL': url})
-    else:
-        print("No data to write to CSV.")
-
-    # Clear crawl_overview_data and links_data for the next iteration
-    crawl_overview_data.clear()
-    links_data.clear()
-    redirects = 0
-    print(len(urls_to_crawl))
-    print(len(urls_crawled))
+      print(url)    
+      # Remove the URL from the urls_to_crawl array
+      if url in urls_to_crawl:
+          urls_to_crawl.remove(url)
+      # Add it to the urls_crawled array
+      urls_crawled.append(url)
+  
+      # get and check links
+      out_links = 0
+      in_links = 0
+      redirect_links = 0
+      links_404 = 0
+      lnx = soup.find_all('a')
+      links_on_page = len(lnx)
+      link_href = []
+      link_anchor = []
+      for link in lnx:
+        l = link.get('href')
+        t = link.get_text()
+        if l.startswith('/'):
+          l = f"{scheme}{root_domain}{l}"
+        if root_domain in l:
+          if l not in urls_to_crawl and l not in urls_crawled:
+            if '#' not in l:
+              if '/wp-content/' not in l:
+                link_url_status = get_url_status(link)
+                urls_to_crawl.add(l)
+          in_links += 1
+        else:
+          out_links += 1
+        if link_url_status == 301 or link_url_status == 302:
+          redirect_links += 1
+        elif link_url_status == 404:
+          links_404 += 1
+        if t and l:
+          link_dict = {
+             'url crawled': url,
+             'link_anchor': t,
+             'link_href': l,
+             'link_url_status': link_url_status
+          }
+  
+          # links to array for export
+          links_data.append(link_dict)
+        
+      # parse the heading tags
+      h1_raw = soup.find_all('h1')
+      h1 = []
+      h2_raw = soup.find_all('h2')
+      h2 = []
+      h3_raw = soup.find_all('h3')
+      h3 = []
+  
+      # extract the strings and add to arrays
+      for h in h1_raw:
+        heading = str(h.string)
+        h1.append(heading)
+      for h in h2_raw:
+        heading = str(h.string)
+        h2.append(heading)
+      for h in h3_raw:
+        heading = str(h.string)
+        h3.append(heading)
+  
+      # Extract page title
+      if soup.title.string:
+        page_title = str(soup.title.string)
+      else:
+        page_title = 'No Title'
+      
+      # Extract all meta tags
+      meta_tags = soup.find_all('meta')
+      meta_tag_names = []
+      meta_content_names = []
+      for meta in meta_tags:
+        if not meta.get('name'):
+          continue
+        else: 
+          meta_tag_names.append(meta.get('name'))
+          meta_content_names.append(meta.get('content'))
+  
+      #output to array
+      crawl_overview_data.append({
+       'url crawled': url,
+       'url_status': url_status,
+       'sitemap_bool': sitemap_bool,
+       'out_links': out_links,
+       'in_links': in_links,
+       'redirect_links': redirects,
+       'links_404': links_404,
+       'page_title': page_title,
+       'h1': h1,
+       'h2': h2,
+       'h3': h3
+      })
+  
+      # Write crawl_overview_data and links_data to CSV files
+      if crawl_overview_data:
+          with open('crawl_overview.csv', 'a', newline='', encoding='utf-8') as file:
+              writer = csv.DictWriter(file, fieldnames=crawl_overview_data[0].keys())
+              if not os.path.isfile('crawl_overview.csv') or os.stat('crawl_overview.csv').st_size == 0:
+                  writer.writeheader()
+              writer.writerows(crawl_overview_data)
+      else:
+          print("No data to write to CSV.")
+    
+      if links_data:
+          with open('links.csv', 'a', newline='', encoding='utf-8') as file:
+              writer = csv.DictWriter(file, fieldnames=links_data[0].keys())
+              if not os.path.isfile('links.csv') or os.stat('links.csv').st_size == 0:
+                  writer.writeheader()
+              writer.writerows(links_data)
+      else:
+          print("No data to write to CSV.")
+  
+      if urls_to_crawl:
+          with open('urls_to_crawl.csv', 'a', newline='', encoding='utf-8') as file:
+              writer = csv.DictWriter(file, fieldnames=['URL'])
+              if not os.path.isfile('urls_to_crawl.csv') or os.stat('urls_to_crawl.csv').st_size == 0:
+                  writer.writeheader()
+              for url in urls_to_crawl:
+                  writer.writerow({'URL': url})
+      else:
+          print("No data to write to CSV.")
+  
+      # Clear crawl_overview_data and links_data for the next iteration
+      crawl_overview_data.clear()
+      links_data.clear()
+      redirects = 0
+      print(len(urls_to_crawl))
+      print(len(urls_crawled))
 
   elif url_status > 200:
     # Remove the URL from the urls_to_crawl array
